b1f87330637fec51268945cd4581e779d967b9e8
jdbrice <jdb12@pdsf8.nersc.gov>
2017-07-10 07:49:21 -0700

    fixed some typos etc. that prevented this running on PDSF

--- Engine.cpp
+++ Engine.cpp
@@ -1,6 +1,8 @@
 
 
 // RooBarb
+#include "Utils.h"
+#include "Logger.h"
 #include "XmlConfig.h"
 #include "TaskEngine.h"
 using namespace jdb;
@@ -18,6 +20,11 @@ using namespace jdb;
 int main( int argc, char* argv[] ) {
 	loguru::add_file("everything.log", loguru::Truncate, loguru::Verbosity_MAX);
 
+	//  I do not understand why but on PDSF
+	// there a re tons of undefined reference errors if these two lines are not included here
+	// I think the compiler optimizes away these implementations if they are not present
+	cout << "test: "  << quote( "hello" ) << endl;
+	Logger::setGlobalLogLevel( "none" );
 
 	TaskFactory::registerTaskRunner<PicoDstSkimmer>( "PicoDstSkimmer" );
 
